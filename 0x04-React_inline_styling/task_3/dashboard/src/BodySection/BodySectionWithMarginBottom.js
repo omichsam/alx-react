5bae773c61b26d9d682eea64b66b9b540658fe43
omichsam <omichsam@gmail.com>
2024-04-19 11:15:02 +0300

    task 3

--- 0x04-React_inline_styling/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -1,34 +1,27 @@
-import React, { Component } from 'react'
-import BodySection from './BodySection';
-import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import BodySection from "./BodySection";
+import { StyleSheet, css } from "aphrodite";
 
 class BodySectionWithMarginBottom extends Component {
   render() {
     return (
-      <div className={css(bodyStyles.marginBottom)}>
-        <BodySection {...this.props}/>
+      <div className={css(styles.bodySectionWithMargin)}>
+        <BodySection {...this.props} />
       </div>
     );
   }
 }
 
-const bodyStyles = StyleSheet.create({
-	marginBottom: {
-		marginBottom: '40px'
-	}
+const styles = StyleSheet.create({
+  bodySectionWithMargin: {
+    marginBottom: "40",
+  },
 });
-
-BodySectionWithMarginBottom. defaultProps = {
-	children: <React.Fragment />
-};
 
 BodySectionWithMarginBottom.propTypes = {
   title: PropTypes.string.isRequired,
-  children: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.element
-  ])
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.element]),
 };
 
 export default BodySectionWithMarginBottom;
--- 0x04-React_inline_styling/task_3/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_3/dashboard/src/CourseList/CourseListRow.js
@@ -1,62 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { StyleSheet, css } from 'aphrodite';
+import React from "react";
+import PropTypes from "prop-types";
+import { StyleSheet, css } from "aphrodite";
 
 function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
-  const headerStyle = {backgroundColor: '#deb5b545'};
-  const rowStyle = {backgroundColor: '#f5f5f5ab'};
-  const selected_style = isHeader ? headerStyle : rowStyle;
-
   return (
-    <tr style={selected_style}>
-      {isHeader ?
-        textSecondCell === null ?
-          <th colSpan="2" className={css(rowsStyles.thcenter)}>{textFirstCell}</th>
-        :
+    <tr className={isHeader ? css(styles.header) : css(styles.normal)}>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th colSpan={2}>{textFirstCell}</th>
+        ) : (
           <>
-            <th className={css(rowsStyles.th)}>{textFirstCell}</th>
-            <th className={css(rowsStyles.th)}>{textSecondCell}</th>
+            <th>{textFirstCell}</th>
+            <th style={headerStyle}>{textSecondCell}</th>
           </>
-      :
+        )
+      ) : (
         <>
-          <td className={css(rowsStyles.td)}>{textFirstCell}</td>
-          <td className={css(rowsStyles.td)}>{textSecondCell}</td>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
         </>
-      }
+      )}
     </tr>
   );
 }
 
-const rowsStyles = StyleSheet.create({
-  thcenter: {
-    borderBottom: '1px solid gray',
-    margin: 0,
-    padding: 0,
-    textAlign: 'center'
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#deb5b545",
   },
-  th: {
-    borderBottom: '1px solid gray',
-    margin: 0,
-    padding: 0,
-    textAlign: 'left'
-  },
-  td: {
-    paddingLeft: 3
-  }
-});
 
-CourseListRow.defaultProps = {
-  isHeader: false,
-  textSecondCell: null
-};
+  normal: {
+    backgroundColor: "#f5f5f5ab",
+  },
+});
 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
-  textFirstCell: PropTypes.string,
-  textSecondCell: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number
-  ])
+  textFirstCell: PropTypes.string.isRequired,
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+};
+
+CourseListRow.defaultProps = {
+  isHeader: false,
+  textSecondCell: null,
 };
 
 export default CourseListRow;
